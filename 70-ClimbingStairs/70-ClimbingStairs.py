ab44f8154ad6ed54f8f847a575ec4fafd9409938
Yogesh-tech2007 <23500529spoc@gmail.com>
2026-08-13 15:54:58 +0530

    Create 70-ClimbingStairs

---
+++ 70-ClimbingStairs/70-ClimbingStairs.py
@@ -0,0 +1,11 @@
+# Last updated: 13/08/2026, 15:54:59
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 0 and n == 1:
+            return 1
+        dp = [0] * (n+1)
+        dp[0] = 1
+        dp[1] = 1
+        for i in range(2,(n+1)):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n]    
